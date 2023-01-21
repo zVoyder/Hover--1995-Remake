b72686fb0f47d6ec4974ac8de33c9f90618b4a39
Denis <zvoyder@protonmail.com>
2023-01-21 15:07:57 +0100

    Optimized AI bounce script
    fixed the AI bounce and optimized

--- Assets/_Scripts/AI/AIBounce.cs
+++ Assets/_Scripts/AI/AIBounce.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using Extension;
 
 /// <summary>
 /// This class is a necessary class tha permits the addforce
@@ -12,13 +11,19 @@ using Extension;
 [RequireComponent( typeof(Rigidbody), typeof(NavMeshAgent), typeof(StateMachineAI) )]
 public class AIBounce : MonoBehaviour
 {
-    public string collisionTag = Extension.Constants.Tags.PLAYER;
-    [Range(5, 50)] public float bumpForce = 10f, pushForce = 5f;
+    [Tooltip("Look at Target when bumped")]public bool lookAtTarget = true;
+    public List<string> collisionTags = new List<string>(){ Extension.Constants.Tags.PLAYER };
+    [Tooltip("Bump force of this rigidbody backward"), Range(5, 50)] public float bumpForce = 10f;
+    [Tooltip("Push force of the rigidbody that collides forward"), Range(5, 50)] public float pushForce = 5f;
     [Tooltip("How long before the AI start to run again?")] [Range(.5f, 5)] public float recover = 1.5f;
+
 
     private Rigidbody _rb;
     private StateMachineAI _en;
     private NavMeshAgent _agent;
+
+    bool _hasCollide;
+    Transform _look;
 
     private void Start()
     {
@@ -27,16 +32,31 @@ public class AIBounce : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision other)
     {
-        if (collision.transform.CompareTag(collisionTag)) // If the collision is with the specific tag
+        if (collisionTags.Contains(other.transform.tag)) // If the collision contains one of the tag in the list
         {
+            _look = other.transform;
             StartCoroutine(RenableIn(recover)); // Start the corutine for recovering after the bounce
             _rb.AddForce(-transform.forward * bumpForce, ForceMode.Impulse); //bump backward
+            
 
-            if(collision.transform.TryGetComponent<Rigidbody>(out Rigidbody rb)){
-                rb.AddForce(-rb.transform.forward * pushForce, ForceMode.Impulse); // push the player backward
+            if(other.transform.TryGetComponent<Rigidbody>(out Rigidbody rb)) // Making sure the object of collision has a rigidbody
+            {
+                
+                rb.AddForce(transform.forward * pushForce, ForceMode.Impulse); // push forward the rigidbody of other collision
             }
+        }
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (lookAtTarget 
+            && _hasCollide)
+        {
+            transform.LookAt(_look.transform);
         }
     }
 
@@ -48,8 +68,11 @@ public class AIBounce : MonoBehaviour
     {
         _agent.enabled = false; 
         _en.enabled = false;
+        _hasCollide = true;
 
         yield return new WaitForSeconds(time); // Wait before renenable
+
+        _hasCollide = false;
 
         _rb.velocity = Vector3.zero; // Set velocity to zero so the navmesh will work properly again
         _en.enabled = true;
